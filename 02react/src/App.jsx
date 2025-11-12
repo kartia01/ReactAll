e5a6555979014421b04368d0978f659a862c5b42
kartia01 <kartia25142@gmail.com>
2025-11-12 18:21:15 +0900

    img

--- 02react/src/App.jsx
+++ 02react/src/App.jsx
@@ -1,23 +1,54 @@
-import { createContext, useContext, useState } from 'react';
+import React, { useState } from 'react';
 
-const Postcontext = createContext();
+function App() {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState('이미지 파일업로드');
+  const [text, setText] = useState('');
 
-export const usePost = () => {
-  const context = useContext(Postcontext);
-  if (!context) {
-    throw new Error('Postprovider 안에 적용해야 함');
-  }
-  return context;
-};
-
-export const PostProvider = ({ children }) => {
-  const [view, setView] = useState('안녕하세요');
-
-  const viewHandler = () => {
-    alert('hi');
+  const fileChangeHandler = (e) => {
+    console.log(e.target.files[0]);
+    const imagefile = e.target.files[0];
+    setFileName(imagefile.name);
+    setFile(imagefile);
   };
 
-  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('image', file);
+    formData.append('test', text);
 
-  return <Postcontext.Provider value={value}>{children}</Postcontext.Provider>;
-};
+    console.log(formData);
+    // alert('전송');
+
+    for (const [key, value] of formData.entries()){
+        console.log(key, value);
+    }
+  };
+
+  return (
+    <div>
+      <h3>파일 업로드</h3>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="file">{fileName}</label>
+          <br />
+          <input type="file" id="file" onChange={fileChangeHandler} multiple />
+        </div>
+        <div>
+          <label htmlFor="testinput">name</label>
+          <input
+            type="text"
+            id="textinput"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit">전송</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
--- 02react/src/App2.jsx
+++ 02react/src/App2.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { UserProvider } from './context/UserContext';
-import ParentComp from './components/context/ParentComp';
+import React, { createContext, useContext, useState } from 'react';
 
-function App2() {
-  return (
-    <UserProvider>
-      <ParentComp />
-    </UserProvider>
-  );
-}
+const UserContext = createContext();
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('userPovider 내부에 있어야 해요');
+  }
+  return context;
+};
 
-export default App2;
+export const UserProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+
+    const signUp = ()=> {
+        
+    }
+};
